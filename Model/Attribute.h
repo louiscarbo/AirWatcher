edebdd48b0fed6b77263665d73d0c7cb52811d64
Darinocan <stephane.s.grondin@gmail.com>
2025-05-16 17:59:46 +0200

    Première version de Measurement et Attribute (à tester)

---
+++ Model/Attribute.h
@@ -0,0 +1,18 @@
+#if ! defined (Attribute)
+#define Attribute
+
+#include <cstring>
+
+
+
+class Measurement {
+    public:
+        
+    protected:
+        string attributeID;
+        string unit;
+        string description;
+
+}
+
+#endif
