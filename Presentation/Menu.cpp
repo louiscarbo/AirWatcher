8a603fde4d78c25f33ae720dc258c3bb5f4516aa
Louis Carbo Estaque <louis@carbo-estaque.fr>
2025-05-27 11:05:05 +0200

    Updated profile selection and main menu display with English translations; added placeholder methods for functionnalities that will not be implemented.

--- Presentation/Menu.cpp
+++ Presentation/Menu.cpp
@@ -29,10 +29,10 @@ void Menu::displayProfileSelectionMenu()
     #ifdef MAP
         cout << "Appel à displayProfileSelectionMenu" << endl;
     #endif
-    cout << "--- Sélection de Profil ---" << endl;
-    cout << "1. Utilisateur" << endl;
-    cout << "2. Administrateur" << endl;
-    cout << "3. Superviseur" << endl;
+    cout << "--- Profile Selection ---" << endl;
+    cout << "1. Private Individual" << endl;
+    cout << "2. Government Agency" << endl;
+    cout << "3. Air Cleaner Provider" << endl;
     cout << "Veuillez sélectionner un profil (1-3): ";
     
     int choice;
@@ -45,11 +45,11 @@ void Menu::displayProfileSelectionMenu()
         if (choice < 1 || choice > 3)
         {
             cin.clear();
-            cout << "Choix invalide. Veuillez entrer un nombre entre 1 et 3: ";
+            cout << "Invalid choice. Please select a valid profile (1-3): ";
         } else {
             validChoice = true;
             selectedProfile = choice;
-            cout << "Profil sélectionné." << endl;
+            cout << "Profile selected." << endl;
         }
     }
 }
@@ -60,19 +60,19 @@ void Menu::displayMainMenu()
         cout << "Appel à displayMainMenu" << endl;
     #endif
     
-    cout << "--- Menu Principal ---" << endl;
+    cout << "--- Menu ---" << endl;
     
     cout << "1. Calculate Air Quality at a Precise Moment and Geography" << endl;
     cout << "2. Calculate the Average Air Quality in a Given Area" << endl;
     cout << "3. Identify Areas with Similar Air Quality" << endl;
     cout << "4. Observe Impact of the Cleaners on Air Quality" << endl;
 
-    if (selectedProfile == 1) // Utilisateur
+    if (selectedProfile == 1) // Private Individual
     {
         cout << "5. Consult your Awarded Points" << endl;
     }
 
-    if (selectedProfile == 2) // Administrateur
+    if (selectedProfile == 2) // Government Agency
     {
         cout << "5. Identify malfunctioning or malicious sensors" << endl;
     }
@@ -112,7 +112,6 @@ Menu::~Menu()
 } //----- Fin de ~Menu
 
 //------------------------------------------------------------------ PRIVE
-//------------------------------------------------------------------ PRIVE
 
 void Menu::selectFunctionality(int option)
 {
@@ -140,12 +139,74 @@ void Menu::selectFunctionality(int option)
                 consultUserPoints();
             } else if (selectedProfile == 2) {
                 identifyMalfunctioningSensors();
+            } else {
+                cout << "Invalid Option. Please try again." << endl;
             }
             break;
         default:
-            cout << "Option invalide. Veuillez réessayer." << endl;
+            cout << "Invalid Option. Please try again." << endl;
             break;
     }
+}
+
+void Menu::calculateAirQualityAtMoment()
+{
+    #ifdef MAP
+        cout << "Appel à calculateAirQualityAtMoment" << endl;
+    #endif
+    
+    cout << "--- Calculate Air Quality at a Precise Moment and Geography ---" << endl;
+    cout << "Not Implemented." << endl;
+}
+
+void Menu::calculateMeanAirQualityArea()
+{
+    #ifdef MAP
+        cout << "Appel à calculateMeanAirQualityArea" << endl;
+    #endif
+    
+    cout << "--- Calculate the Average Air Quality in a Given Area ---" << endl;
+    cout << "A faire." << endl;
+}
+
+void Menu::identifyAreasWithSimilarAirQuality()
+{
+    #ifdef MAP
+        cout << "Appel à identifyAreasWithSimilarAirQuality" << endl;
+    #endif
+    
+    cout << "--- Identify Areas with Similar Air Quality ---" << endl;
+    cout << "Not Implemented." << endl;
+}
+
+void Menu::identifyMalfunctioningSensors()
+{
+    #ifdef MAP
+        cout << "Appel à identifyMalfunctioningSensors" << endl;
+    #endif
+    
+    cout << "--- Identify malfunctioning or malicious sensors ---" << endl;
+    cout << "A faire." << endl;
+}
+
+void Menu::observeCleanerImpact()
+{
+    #ifdef MAP
+        cout << "Appel à observeCleanerImpact" << endl;
+    #endif
+    
+    cout << "--- Observe Impact of the Cleaners on Air Quality ---" << endl;
+    cout << "Not Implemented." << endl;
+}
+
+void Menu::consultUserPoints()
+{
+    #ifdef MAP
+        cout << "Appel à consultUserPoints" << endl;
+    #endif
+    
+    cout << "--- Consult User Points ---" << endl;
+    cout << "Not Implemented." << endl;
 }
 
 //----------------------------------------------------- Méthodes protégées
