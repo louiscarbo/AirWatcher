83ebd9e6fb08248946387e0163315878041353f4
Darinocan <stephane.s.grondin@gmail.com>
2025-05-27 10:58:14 +0200

    Création d'une classe PrivateIndividual vide pour eviter les erreurs de compilation

---
+++ Model/PrivateIndividual.h
@@ -0,0 +1,57 @@
+/*************************************************************************
+                           Classe  -  PrivateIndividual
+*************************************************************************/
+
+//- Interface de la classe <PrivateIndividual> (fichier PrivateIndividual.h)
+#ifndef PRIVATEINDIVIDUAL_H
+#define PRIVATEINDIVIDUAL_H
+//--------------------------------------------------- Interfaces utilisées
+
+//------------------------------------------------------------- Constantes
+
+//------------------------------------------------------------------ Types
+
+//------------------------------------------------------------------------
+// Rôle de la classe PrivateIndividual
+//
+//
+//------------------------------------------------------------------------
+
+
+
+
+class PrivateIndividual {
+    //----------------------------------------------------------------- PUBLIC
+    public:
+        //----------------------------------------------------- Méthodes publiques
+
+
+        //------------------------------------------------- Surcharge d'opérateurs
+
+        //-------------------------------------------- Constructeurs - destructeur
+
+        
+        PrivateIndividual();
+        // Mode d'emploi :
+        //
+        // Contrat :
+        //
+
+        ~PrivateIndividual();
+        // Mode d'emploi :
+        //
+        // Contrat :
+        //
+
+    //------------------------------------------------------------------ PRIVE
+    protected:
+    //----------------------------------------------------- Méthodes protégées
+
+    //----------------------------------------------------- Attributs protégés
+
+
+};
+
+//-------------------------------- Autres définitions dépendantes de <PrivateIndividual>
+
+#endif // PRIVATEINDIVIDUAL_H
